9d6be9978aa7a566a88c63c708ba339c95fe9ec5
priyanka-kale <kalepriya100@gmail.com>
2020-10-14 22:07:01 +0530

    priynka kale commited

--- api/nodemon.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "JWT_KEY"   : "secret",
-    "port"      : 3066,
-    "dbname"    : "standardapi",
-    "emailHost" : "smtp.gmail.com",
-    "emailPort" : 587,
-    "user"      : "iassureitmail@gmail.com",
-    "pass"      : "iAssureIT@123"
-}
