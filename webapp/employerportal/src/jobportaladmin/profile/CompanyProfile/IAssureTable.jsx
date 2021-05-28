ccb43fc83dd3217d28aeaa5202dc71e994ce8dce
Sanket kutwal <sanketkutwal325@gmail.com>
2021-05-28 10:33:05 +0530

    Sanket Kutwal commited code

--- webapp/employerportal/src/jobportaladmin/profile/CompanyProfile/IAssureTable.jsx
+++ webapp/employerportal/src/jobportaladmin/profile/CompanyProfile/IAssureTable.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route, withRouter } from 'react-router-dom';
-import swal from 'sweetalert';
+import Swal                   from 'sweetalert2';
 import axios from 'axios';
 import $ from 'jquery';
 import jQuery from 'jquery';
@@ -120,10 +120,7 @@ class IAssureTable extends Component {
 		}).then((response) => {
 			this.props.getData(this.state.startRange, this.state.limitRange);
 			this.props.history.push(tableObjects.editUrl);
-			swal({
-				title : " ",
-				text  : "Record deleted successfully",
-			});
+			Swal.fire('', "Record deleted successfully", '');
 		}).catch((error) => {
 		});
 	}
